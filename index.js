e483cc38a2674620f9f965876dd67e29b45bf810
Venkatesan <venkatesangunaraj@gmail.com>
2025-02-02 02:18:53 +0530

    🐛 fix: resolve bug

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import express from "express";
-import { PrismaClient } from "@prisma/client";
+const express = require("express");
+const { PrismaClient } = require("@prisma/client");
 
 const app = express();
 const prisma = new PrismaClient();
